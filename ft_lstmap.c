e512fed19f6fa49fff8ea9047a2b2c15ede1e21e
Eridor77 <ruddy.lavalloir@gmail.com>
2016-09-07 16:32:55 +0200

    Implemented

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -2,5 +2,21 @@
 
 t_list *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	
+	t_list *plist;
+	t_list *nlist;
+
+	if (!lst)
+		return (NULL);
+	nlist = ft_lstnew(lst->content, lst->content_size);
+	if (!nlist)
+		return (NULL);
+	nlist = f(lst);
+	plist = nlist;
+	while (lst->next)
+	{
+		plist->next = f(lst->next);
+		plist = plist->next;
+		lst = lst->next;
+	}
+	return (nlist);
 }
